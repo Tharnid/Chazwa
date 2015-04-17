5909789ab871b62874254507d493740fc69b0f28
Tharnid <jeffmsmithdev@gmail.com>
2015-04-17 16:01:52 -0400

    done with customer class

---
+++ OOCSharp/ACM.BL.Test/CustomerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BL.Test
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ OOCSharp/TCM.BL/Customer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCM.BL
+{
+    public class Customer
+    {
+        public static int InstanceCount { get; set; }
+
+        private string _lastName;
+        
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        public string FirstName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public int CustomerId { get; private set; }
+
+        // public int MyProperty { get; private set; } propg + TAB = property with a privat setter
+
+
+        public string FullName
+        {
+            get
+            {
+                string fullName = LastName;  // internal means code is internal to the component in which it is defined
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(fullName))
+                    {
+                        FullName += ", ";
+                    }
+                    FullName += FirstName;
+                }
+                return FullName;
+            }
+            set
+            {
+                // nothing to see here
+            }
+        }
+        
+    }
+}
