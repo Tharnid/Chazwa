62d21b91b19cb815518ce2e485bce59df70ad36c
Tharnid <jeffmsmithdev@gmail.com>
2015-05-13 16:06:35 -0400

    done with project setup

---
+++ AngDotNet/AngDotNet.Web/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AngDotNet.Web.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        NWEntities _db = new NWEntities();
+
+        public IEnumerable<Customer> getAllCustomers()
+        {
+            _db.Configuration.ProxyCreationEnabled = false;
+            return _db.Customers;
+        }
+
+        public IHttpActionResult getCustomerByCustomerID(string customerID)
+        {
+            _db.Configuration.ProxyCreationEnabled = false;
+
+            var customer = _db.Customers.FirstOrDefault(c =>
+                           c.CustomerID == customerID);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return Ok(customer);
+        }
+    }
+}
---
+++ AngDotNet/AngDotNet.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngDotNet.Web.WebApiApplication" Language="C#" %>
