f4356ac5d0c0aafe47767306354498c852516afb
Tharnid <jeffmsmithdev@gmail.com>
2015-05-28 17:49:43 -0400

    moving along

---
+++ CSharpBeg/Bananas/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bananas
+{
+    class Program
+    {
+        static void Main(string[] args) // scope is the portion of program to which the variable is referred to by name
+        {
+            var numberOfBananas = 3;
+            Console.WriteLine("Number of bananas: " + " " + numberOfBananas);
+            var numberOfApples = 2;
+            Console.WriteLine("Number of apples: " + " " + numberOfApples);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpBeg/Fruit2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fruit2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numOfGrapes = 6;
+            var numOfKiwis = 5;
+            var numOfPapayas = 4;
+
+            Console.WriteLine("Number of Grapes: " + " " + numOfGrapes);
+            Console.WriteLine("Number of Kiwi's: " + " " + numOfKiwis);
+            Console.WriteLine("Number of Papaya's: " + " " + numOfPapayas);
+            Console.ReadLine();
+        }
+    }
+}
