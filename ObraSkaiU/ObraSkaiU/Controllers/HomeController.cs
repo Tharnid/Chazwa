6e5b0d1641c3ead9a8fba00359d8bcb137c3a81c
Tharnid <jeffmsmithdev@gmail.com>
2015-06-02 14:20:33 -0400

    done with filters, paging and sorting

--- ObraSkaiU/ObraSkaiU/Controllers/HomeController.cs
+++ ObraSkaiU/ObraSkaiU/Controllers/HomeController.cs
@@ -3,21 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ObraSkaiU.DAL;
+using ObraSkaiU.ViewModels;
 
 namespace ObraSkaiU.Controllers
 {
     public class HomeController : Controller
     {
+        // put data contexts here
+        private SchoolContext db = new SchoolContext();
+
         public ActionResult Index()
         {
             return View();
         }
 
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            IQueryable<EnrollmentDateGroup> data = from student in db.Students
+                                                   group student by student.EnrollmentDate into dateGroup
+                                                   select new EnrollmentDateGroup()
+                                                   {
+                                                       EnrollmentDate = dateGroup.Key,
+                                                       StudentCount = dateGroup.Count()
+                                                   };
+            return View(data.ToList());
         }
 
         public ActionResult Contact()
@@ -25,6 +42,12 @@ namespace ObraSkaiU.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
