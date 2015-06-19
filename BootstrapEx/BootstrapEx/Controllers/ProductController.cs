a7b284be4704baa712662378ed66f3a6430ba163
Jeff Smith <jeffmsmithdev@gmail.com>
2015-06-19 12:03:09 -0400

    BootstrapEx - added StructureMap and AutoMapper

---
+++ BootstrapEx/BootstrapEx/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootstrapEx.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+        private readonly ICurrentUser _currentUser;
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
