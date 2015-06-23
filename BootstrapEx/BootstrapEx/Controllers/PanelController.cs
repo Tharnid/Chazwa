e302649f7f95bf85db6689bef9a3dfe776f566c0
Jeff Smith <jeffmsmithdev@gmail.com>
2015-06-23 11:37:29 -0400

    done with panels

--- BootstrapEx/BootstrapEx/Controllers/PanelController.cs
+++ BootstrapEx/BootstrapEx/Controllers/PanelController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SamplesData;
 
 namespace BootstrapEx.Controllers
 {
@@ -10,6 +11,16 @@ namespace BootstrapEx.Controllers
     {
         // GET: Panel
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult P1()
+        {
+            return View();
+        }
+
+        public ActionResult P2()
         {
             return View();
         }
