912f80b6e37a4fb365f52b8ece1f64935a64a06e
Jeff Smith <jeffmsmithdev@gmail.com>
2015-06-23 10:55:29 -0400

    moving on to panel additions

--- BootstrapEx/BootstrapEx/Controllers/HomeController.cs
+++ BootstrapEx/BootstrapEx/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SamplesData;
 
 namespace BootstrapEx.Controllers
 {
@@ -54,7 +55,9 @@ namespace BootstrapEx.Controllers
 
         public ActionResult DDL2()
         {
-            return View();
+            MusicGenre entity = new MusicGenre();
+
+            return View(entity);
         }
     }
 }
