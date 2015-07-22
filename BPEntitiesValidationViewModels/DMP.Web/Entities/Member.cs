a7985ad2780c112e95dea2efe36c5af273a864ff
Jeff Smith <jeffmsmithdev@gmail.com>
2015-07-22 09:34:53 -0400

    on to collection attributes

---
+++ BPEntitiesValidationViewModels/DMP.Web/Entities/Member.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+
+namespace DMP.Web.Entities
+{
+    public class Member
+    {
+        public int MemberId { get; set; }
+
+        public string Username { get; set; }
+        public DateTime LastLogin { get; set; }
+        public DateTime Created { get; set; }
+
+        public Profile Profile  { get; set; }
+    }
+}
