1a571e38879a0cc3fe82669b098f98f66f2b0e91
Jeff Smith <jeffmsmithdev@gmail.com>
2015-07-31 12:38:26 -0400

    done with vid 4

---
+++ KVMVC/Basics/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Basics.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public String Index(string id)
+        {
+            // return View();
+            // return "Hello from MVC Application!!!";
+            // return "Id = " + id; // pass that by going to Home/Index/XXXX XXXX being whatever number
+            // you can see the trace information by typing trace.axd
+            return "Id = " + id + " " + "Name = " + Request.QueryString["name"];  // passed through URL
+        }
+
+        public string GetDetails()
+        {
+            return "GetDetails Invoked!!!";
+        }
+    }
+}
