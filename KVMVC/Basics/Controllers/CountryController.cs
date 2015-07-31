5935f548c1e84b7e9ee97a91d8f71e5ee39408bf
Jeff Smith <jeffmsmithdev@gmail.com>
2015-07-31 13:03:58 -0400

    hmmm

---
+++ KVMVC/Basics/Controllers/CountryController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Basics.Controllers
+{
+    public class CountryController : Controller
+    {
+        
+        // List
+        public ActionResult Index()  // views inherit from action result
+        {
+            return new List<string>()  // returns name of the type
+            {
+                "India",
+                "US",
+                "UK",
+                "Canada"
+            };
+
+            return View();
+        }
+    }
+}
