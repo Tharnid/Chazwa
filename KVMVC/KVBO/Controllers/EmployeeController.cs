80061fd90ba33f4aa8cb318be699cadabf09486c
Jeff Smith <jeffmsmithdev@gmail.com>
2015-08-04 09:52:56 -0400

    done with employee/create

---
+++ KVMVC/KVBO/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer;
+
+namespace KVBO.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            EmployeeBusinessLayer ebl = new EmployeeBusinessLayer();
+
+            List<Employee> emp = ebl.Employees.ToList();
+
+            return View(emp);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
