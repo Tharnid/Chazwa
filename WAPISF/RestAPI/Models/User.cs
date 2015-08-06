b80824aa91f013e589856ed1a031d2147345bf63
Tharnid <jeffmsmithdev@gmail.com>
2015-08-06 11:28:49 -0400

    web api

---
+++ WAPISF/RestAPI/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestAPI.Models
+{
+    public class User
+    {
+        public long UserId { get; set; }
+        public string Username { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public List<Link> Links { get; set; }
+    }
+
+}
