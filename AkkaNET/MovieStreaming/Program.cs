d9968a9dbaae0367f6cde316a2d07ec5f4f190ef
Tharnid <jeffmsmithdev@gmail.com>
2015-08-18 16:25:17 -0400

    moving on

---
+++ AkkaNET/MovieStreaming/Program.cs
@@ -0,0 +1,23 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieStreaming
+{
+    class Program
+    {
+        private static ActorSystem MovieStreamingActorSystem;
+
+        static void Main(string[] args)
+        {
+            MovieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
+
+            Console.ReadLine();
+
+            MovieStreamingActorSystem.Shutdown();
+        }
+    }
+}
---
+++ EF6JL/NinjaDomain.Classes/NinjaEquipment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using NinjaDomain.Classes.Enums;
+
+namespace NinjaDomain.Classes
+{
+    public class NinjaEquipment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public EquipmentType Type { get; set; }
+        [Required]
+        public Ninja Ninja { get; set; }
+    }
+}
