235d22d110449cbe0df176efe917b37febd18f3a
Tharnid <jeffmsmithdev@gmail.com>
2015-10-16 10:39:12 -0400

    working on ajsforms

--- AngFormsMVC/AngularForms/app/EmployeeForm/efController.js
+++ AngFormsMVC/AngularForms/app/EmployeeForm/efController.js
@@ -1,3 +1,14 @@
 ﻿angularFormsApp.controller('efController', function efController($scope, efService) {
     $scope.employee = efService.employee;
+
+    $scope.departments = [
+    "Engineering",
+    "Procurement",
+    "Finance",
+    "Administration"
+    ];
+
+    $scope.submitForm = function () {
+
+    }
 });
