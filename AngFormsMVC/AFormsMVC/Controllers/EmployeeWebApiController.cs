bdece9a9f9ed244f346c9b6fbe5eb4db649d0a6c
Tharnid <jeffmsmithdev@gmail.com>
2015-12-03 12:46:07 -0500

    sad...solution broken after last section

---
+++ AngFormsMVC/AFormsMVC/Controllers/EmployeeWebApiController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AFormsMVC.Models;
+
+
+namespace AFormsMVC.Controllers
+{
+    public class EmployeeWebApiController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<EmployeeVM> Get()
+        {
+            List<EmployeeVM> list = new List<EmployeeVM>()
+            {
+                new EmployeeVM() {
+                    FullName = "Milton Waddams"
+                },
+                new EmployeeVM() {
+                    FullName = "Andy Bernard"
+                }
+            };
+            return list;
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
