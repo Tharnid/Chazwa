d0f289e7af6420f16ddcec18cc1f329332a3d6f9
Tharnid <jeffmsmithdev@gmail.com>
2015-12-07 17:10:09 -0500

    starting C# Studies

---
+++ CSharpStudies/Arith2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arith2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool m;
+
+            m = 30 - 10 + 20 * 4 > 100 - 1;
+
+            // Floating Values
+
+            // Decimal literals must end in m
+            decimal dollars = 123.45m;
+
+            // double literals typically do not
+            double width = 240.0d;
+            double length = 156.7;
+            double huge = 1.235689e13;
+
+            // Mixing integers with dobules or decimals is fine
+            double triangleArea = width * length / 2;
+
+            // Can't mix decimals with doubles
+            //decimal pricePerMeter = width * length;
+
+
+
+            // Type cast tells c# that you really want to mix them, and how:
+
+            decimal pricePerMeter = dollars / (decimal)length;
+
+            double oneTenth = 0.1;
+            double one = oneTenth + oneTenth + oneTenth + oneTenth + oneTenth + oneTenth + oneTenth + oneTenth + oneTenth + oneTenth;
+
+            decimal oneThird = 1.0m / 3.0m;
+
+            Console.WriteLine(oneThird + oneThird + oneThird);
+
+            Console.WriteLine(m);
+            Console.WriteLine(triangleArea);
+            Console.WriteLine(pricePerMeter);
+            Console.WriteLine(1.0 == one);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpStudies/IntArith/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntArith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            i = 30 - 10 + 20 * 4;
+
+            int h;
+
+            h = 23 / 2;
+
+            // unary negative
+
+            int g;
+
+            g = -23 / 4;
+
+            // remainder gives what is left
+
+            int j;
+            int k;
+
+            j = 23 % 4;
+            k = -23 % 4;
+
+            bool l = 23 < 4;
+
+            //string x;
+            //string y;
+
+            //x < y; less than 
+            //x < y; greater than
+            //x <= y; less than equals to
+            //x >= y; greater than equal to
+            //x == y; equal
+            //x != y; NotEqual
+
+            Console.WriteLine(l);
+            Console.WriteLine(i);
+            Console.WriteLine(h);
+            Console.WriteLine(g);
+            Console.WriteLine(j);
+            Console.WriteLine(k);
+            Console.ReadLine();
+        }
+    }
+}
