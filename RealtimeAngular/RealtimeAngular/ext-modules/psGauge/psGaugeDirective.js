1e70b6ad11cdc5f071a723afbe70ab689d6ac5d7
Tharnid <jeffmsmithdev@gmail.com>
2016-01-20 16:03:30 -0500

    moving on to github library for the charts

--- RealtimeAngular/RealtimeAngular/ext-modules/psGauge/psGaugeDirective.js
+++ RealtimeAngular/RealtimeAngular/ext-modules/psGauge/psGaugeDirective.js
@@ -5,6 +5,21 @@ angular.module('psGauge').directive('psGauge', function () {
         templateUrl: 'ext-modules/psGauge/psGaugeTemplate.html',
         link: function (scope, el, attrs) {
 
+            scope.options = {
+                width: 200, height: 200,
+                redFrom: 90, redTo: 100,
+                yellowFrom: 75, yellowTo: 90,
+                minorTicks: 5
+            };
+
+            scope.data = google.visualization.arrayToDataTable([
+                ['Label', 'Value'],
+                ['CPU %', 0]
+            ]);
+
+            var chart = new google.visualization.Gauge(el[0]);
+
+            chart.draw(scope.data, scope.options);
         }
     };
 });
