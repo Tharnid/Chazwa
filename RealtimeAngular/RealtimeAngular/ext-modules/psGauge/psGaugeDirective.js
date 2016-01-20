46f920545234bb2a15f030cdc8c9d7751da35f42
Tharnid <jeffmsmithdev@gmail.com>
2016-01-20 15:23:30 -0500

    moving on to google charts

---
+++ RealtimeAngular/RealtimeAngular/ext-modules/psGauge/psGaugeDirective.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+
+angular.module('psGauge').directive('psGauge', function () {
+    return {
+        templateUrl: 'ext-modules/psGauge/psGaugeTemplate.html',
+        link: function (scope, el, attrs) {
+
+        }
+    };
+});
