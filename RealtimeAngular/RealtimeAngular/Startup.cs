bb7d4080c8d79e3c48f8656858750f5417686669
Tharnid <jeffmsmithdev@gmail.com>
2016-01-20 16:20:18 -0500

    on to signalR background Timer

---
+++ RealtimeAngular/RealtimeAngular/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(RealtimeAngular.Startup))]
+
+namespace RealtimeAngular
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
