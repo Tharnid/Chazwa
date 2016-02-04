eb839bbc6d1d4a33ddb339cc5ffd850521dddf98
Tharnid <jeffmsmithdev@gmail.com>
2016-02-04 16:31:08 -0500

    wow

---
+++ TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,36 @@
+﻿// site.js
+// Patterns in JavaScript
+
+// Self executing anonymous function
+
+//function startup() {
+//    var ele = document.getElementById("username");
+//    ele.innerHTML = "Han Solo";
+
+//    var main = document.getElementById("main");
+//    main.onmouseenter = function () {
+//        main.style = "background-color: #888;";
+//    };
+
+//    main.onmouseleave = function () {
+//        main.style = "";
+//    };
+//};
+
+// startup();
+
+// immediately invoked function expression (iife)
+
+(function () { // (inputParam)
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Han Solo";
+
+    var main = document.getElementById("main");
+    main.onmouseenter = function () {
+        main.style = "background-color: #888;";
+    };
+
+    main.onmouseleave = function () {
+        main.style = "";
+    };
+})(); // you can put params in here....("Hello")
