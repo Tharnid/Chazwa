9f261986003463f72b9d6d535eca735582ce812f
Tharnid <jeffmsmithdev@gmail.com>
2016-02-05 10:02:16 -0500

    on to mvc6

--- TheWorld/src/TheWorld/wwwroot/js/site.js
+++ TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -37,21 +37,34 @@
 
 // JQuery version
 
+//(function () {
+//    var ele = $("#username");
+//    ele.text("Han Solo");
+
+//    var main = $("#main");
+//    main.on("mouseenter", function () {
+//        main.style = "background-color: #888;";
+//    });
+
+//    main.on("mouseleave", function () {
+//        main.style = "";
+//    });
+
+//    var menuItems = $("ul.menu li a");
+//    menuItems.on("click", function () {
+//        alert("Hello!!!");
+//    })
+//})();
+
 (function () {
-    var ele = $("#username");
-    ele.text("Han Solo");
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
 
-    var main = $("#main");
-    main.on("mouseenter", function () {
-        main.style = "background-color: #888;";
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
     });
-
-    main.on("mouseleave", function () {
-        main.style = "";
-    });
-
-    var menuItems = $("ul.menu li a");
-    menuItems.on("click", function () {
-        alert("Hello!!!");
-    })
 })();
