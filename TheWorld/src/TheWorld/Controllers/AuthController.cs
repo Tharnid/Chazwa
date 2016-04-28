821ac148d6e10a288e5204d606a01bf1c6186ba4
Tharnid <jeffmsmithdev@gmail.com>
2016-04-28 12:41:17 -0400

    oops

---
+++ TheWorld/src/TheWorld/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheWorld.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Trips", "App");  // Action || Controller
+            }
+
+            return View();
+        }
+    }
+}
