f843684ee79214f06e673fd18a77232d3c8c56d7
Tharnid <jeffmsmithdev@gmail.com>
2017-01-15 22:08:02 -0500

    onto 3

---
+++ Secode2/src/Secode2/wwwroot/file.js
@@ -0,0 +1,13 @@
+var stringVariable;
+var boolVar;
+var dateVar;
+var anyVar; // default fallback in javascript only use this whenver necessary
+var numberVar;
+var dh;
+var stringVariable2 = "";
+var anItem = {
+    item: ""
+};
+var someVar = anItem.item;
+// var anyVar = <string>anItem.item;    
+//# sourceMappingURL=file.js.map
---
+++ Secode2/src/Secode2/wwwroot/file.ts
@@ -0,0 +1,19 @@
+﻿var stringVariable: string;
+var boolVar: boolean;
+var dateVar: Date;
+var anyVar: any;  // default fallback in javascript only use this whenver necessary
+
+var numberVar: number;
+
+var dh: any;
+
+var stringVariable2 = "";
+
+var anItem: any = {
+    item: ""
+}
+
+var someVar: string = anItem.item;
+// var anyVar = <string>anItem.item;    
+
+
