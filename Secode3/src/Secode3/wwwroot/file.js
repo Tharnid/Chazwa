f9eb72c9a86506364a0467d8f7674a4a0614617d
Tharnid <jeffmsmithdev@gmail.com>
2017-01-22 13:30:49 -0500

    done with person card

--- Secode3/src/Secode3/wwwroot/file.js
+++ Secode3/src/Secode3/wwwroot/file.js
@@ -1,18 +1,44 @@
 var Person = (function () {
-    function Person() {
+    function Person(lastName) {
+        if (lastName === void 0) { lastName = "[Last Name]"; }
     }
     Person.prototype.showMe = function () {
         var div = document.createElement("div");
         div.innerHTML = JSON.stringify(this);
         document.getElementsByTagName("body")[0].appendChild(div);
     };
+    Person.prototype.makeFullName = function () {
+        this._fullName = '${this.firstName} ${this.lastName}';
+        return this._fullName;
+    };
+    Person.prototype.setAge = function (age) {
+        this.age = age;
+    };
+    Person.prototype.setAgeAndDate = function (age, birthDate) {
+        this.age = age;
+    };
+    Person.prototype.setNames = function (lastName) {
+        if (lastName === void 0) { lastName = "Solo"; }
+    };
+    Person.prototype.setSibling = function (sibling) {
+        this._sibling = new Person();
+        this._sibling.firstName = sibling.firstName;
+        this._sibling.lastName = sibling.lastName;
+    };
     return Person;
 }());
 var p = new Person();
+var secondPerson = new Person();
 p.firstName = "Corran";
 p.lastName = "Horn";
 p.age = 23;
 p.birthDate = new Date();
+//alert(p.makeFullName());
+p.setAgeAndDate(23);
+p.setAgeAndDate(23, new Date());
+p.setNames();
+p.setNames("Does");
+p.setSibling({ age: 12, lastName: "Fortuna", firstName: "Bib" });
 p.showMe();
 // alert(JSON.stringify(p)); 
 //# sourceMappingURL=file.js.map
--- Secode3/src/Secode3/wwwroot/file.ts
+++ Secode3/src/Secode3/wwwroot/file.ts
@@ -2,21 +2,64 @@
     firstName: string;
     lastName: string;
     age: number;
-    birthDate: Date 
+    birthDate: Date;
+
+    private _fullName: string;
 
     showMe(): void {
         var div = document.createElement("div");
         div.innerHTML = JSON.stringify(this);
         document.getElementsByTagName("body")[0].appendChild(div);
     }
+
+    makeFullName(): string {
+        this._fullName = '${this.firstName} ${this.lastName}';
+        return this._fullName;
+    }
+
+    setAge(age: number) {
+        this.age = age;
+    }
+
+    setAgeAndDate(age: number, birthDate?: Date) {
+        this.age = age;
+    }
+
+    setNames(lastName: string = "Solo") {
+
+    }
+
+    private _sibling: Person;
+
+    setSibling(sibling: { age: number, lastName: string, firstName: string }) {
+        this._sibling = new Person();
+        this._sibling.firstName = sibling.firstName;
+        this._sibling.lastName = sibling.lastName;
+    }
+
+    constructor(lastName = "[Last Name]") {
+
+    }
 }
 
 var p = new Person();
+
+var secondPerson = new Person();
 
 p.firstName = "Corran";
 p.lastName = "Horn";
 p.age = 23;
 p.birthDate = new Date();
+
+//alert(p.makeFullName());
+
+p.setAgeAndDate(23);
+p.setAgeAndDate(23, new Date());
+
+p.setNames();
+p.setNames("Does");
+
+p.setSibling({ age: 12, lastName: "Fortuna", firstName: "Bib" });
 
 p.showMe();
 
